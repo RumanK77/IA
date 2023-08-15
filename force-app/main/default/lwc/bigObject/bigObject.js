70f2fa5e714c929613b486664205a4f3fef2a830
RumanK77 <rumankadri77@gmail.com>
2023-08-15 22:45:45 +0530

    Unwanted Components Deleted

--- force-app/main/default/lwc/bigObject/bigObject.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-
-export default class BigObject extends LightningElement {
-    
-}
--- force-app/main/default/lwc/loanPathAssistant/loanPathAssistant.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class LoanPathAssistant extends LightningElement {
-    @api recordId;
-    currentStep = "New";
-}
